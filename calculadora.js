b1971f6f7509b662fecd11dea79bfa162d67def3
Maysafig <maysafig@gmail.com>
2022-04-13 21:45:03 -0300

    Exercícios resolvidos em aula na quarta-feira

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,25 @@
 //Vamos criar uma calculadora com as 4 operações matemáticas básicas? rs
+/*a) Deverá ser possível escolher uma operação aritimética em forma de string: 'soma', 'multiplicacao', 'divisao' e 'subtracao'
+b) Deverá ser possível passar 2 números para a operação escolhida
+c) Deverá retornar o resultado e imprimir no console*/
+
+function calcula(operador, num1, num2){
+    let resultado
+    if (operador === 'soma'){
+        resultado = num1 + num2
+        return  resultado
+    } else if(operador == 'multiplicacao'){
+        resultado = num1 * num2
+        return resultado
+    } else if (operador == 'divisao'){
+        resultado = num1 / num2
+        return resultado
+    } else if (operador == 'subtracao'){
+        resultado = num1 - num2
+        return resultado
+    } else {
+        return `${operador} inválido`
+    }
+}
+
+console.log(calcula('soma', 20, 55)) //75
--- parOuImpar.js
+++ parOuImpar.js
@@ -1,37 +1,12 @@
 //Crie uma função que determina se um número é par ou impar.
 
-
-/*function retornarParouImpar(numero){
-    switch(numero){
-        case 1 (numero % 2 === 0)':
-            return 'É par'
-        default:
-            return 'É impar'
+function determinaParOuImpar(numero){
+    if (numero % 2 == 0 ){
+        return `${numero} é par.`
+    } else {
+        return `${numero} é ímpar`
     }
 }
 
-console.log(retornarParouImpar(8))*/
-
-/*let x=10%2
-switch(x){
-  case 0:
-    console.log("its even number")
-    break;
-  case 1:
-    console.log("its odd number")
-    break;
-}
-
-let verificaParOuImpar = 10 % 2
-
-switch(verificaParOuImpar) = */
-
-let numero
-let verificaParOuImpar = numero % 2 == 0
-
-switch(verificaParOuImpar){
-    case numero = 10:
-        return 'É par'
-    case false: 
-        return 'É impar'
-}
+const resultado = determinaParOuImpar(10) //é par ou ímpar
+console.log(resultado)
